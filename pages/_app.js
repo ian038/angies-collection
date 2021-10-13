8563b2e0a09bdf07beb727b5fd4fd60aa405aa6c
ian038 <ianphua3@gmail.com>
2021-10-13 12:29:43 -0400

    Created home page and navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
 import '../styles/globals.css'
+import Link from 'next/link'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <nav className='border-b p-6'>
+        <p className='text-4xl font-bold'>Angies Collection</p>
+        <div className='flex mt-4'>
+          <Link href='/add-item'>
+            <a className='mr-6 text-blue-700'>Add new NFT</a>
+          </Link>
+          <Link href='/'>
+            <a className='mr-6 text-blue-700'>My NFTs</a>
+          </Link>
+        </div>
+      </nav>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
